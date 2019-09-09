b4e845cf811909a0571fac9462135877ba1a81e9
JenniKate Wallace <jennikate@gmail.com>
2019-09-09 14:51:17 +0100

    extra bits

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -24,6 +24,9 @@ var cards = [
   }
 ]
 var cardsInPlay = []
+var gameOutcomeText //show win or lose text
+var gameOutcome //change color of text based on win or lose
+
 
 function createBoard(){
   for (var i=0; i<cards.length; i++){
@@ -38,9 +41,13 @@ function createBoard(){
 function checkForMatch(){
   console.log(cardsInPlay.length);
     if (cardsInPlay[0] === cardsInPlay[1]) {
-      alert("You found a match");
+      //alert("You found a match");
+      gameOutcomeText = "You found a match!";
+      gameOutcome = "win";
     } else {
-      alert("Sorry, try again");
+      //alert("Sorry, try again");
+      gameOutcomeText = "Sorry try again";
+      gameOutcome = "lose";
     }
 };
 
@@ -50,26 +57,41 @@ function flipCard(){
   cardsInPlay.push(cards[cardId].rank);
   console.log(cards[cardId].cardImage);
   console.log(cards[cardId].suit);
-
   this.setAttribute('src', cards[cardId].cardImage);
 
   if(cardsInPlay.length === 2) {
     checkForMatch();
+    //result text and button
+    document.getElementById("game-result").textContent = gameOutcomeText;
+    document.getElementById("game-result").setAttribute("class", gameOutcome);
+    document.getElementById("reset-cards").setAttribute("class", "show");
+    document.getElementById("reset-score").setAttribute("class", "show");
   } else {
     console.log("not two");
+
     }
 };
 
+// Reset cards
+function cardReset(){
+  var gameBoard = document.getElementById("game-board");
+  while (gameBoard.hasChildNodes()) {
+    gameBoard.removeChild(gameBoard.firstChild);
+  };
+};
 
+//reset the game
+function gameReset(){
+  document.getElementById("reset-cards").setAttribute("class", "hide"); //hides reset button
+  document.getElementById("reset-score").setAttribute("class", "hide"); //hides reset button
+  document.getElementById("game-result").textContent = ""; //hides win/lose text
+  cardsInPlay = []; //sets back to no cards selected
+  cardReset();
+  createBoard();
+};
 
-
-
-
-
-
-
-
-
+document.getElementById("reset-cards").addEventListener("click", gameReset);
+//document.getElementById("reset-score").addEventListener("click", scoreReset);
 createBoard();
 
 /*
